8ac7729f3083c4c29db6bd3bec3697baa9f8abe9
diman <diman@DIMA-PC>
2021-06-22 13:26:35 +0300

    Добавьте файлы проекта.

---
+++ Lab7/main.cpp
@@ -0,0 +1,21 @@
+#include "header_lab7.h"
+
+int main() {
+	
+	setlocale(LC_ALL, "Russian");
+
+	ifstream fin;
+	list<supplier> l;
+	fin.open("data.txt");
+	if (!fin.is_open()) {
+		cout << "Файла нет, инициализация с клавиатуры!" << endl;
+		l = add_supplier(l, 1, 2);
+	}
+	else {
+		l = load_supplier(l);
+	}
+	list_supplier(l);
+	user_interface(l);
+
+	return 0;
+}
